73d5194aedeeb5b9018245938d421b5845fbd60d
BLACKJALS <jholeon@uan.edu.co>
2015-05-01 20:04:22 -0500

    ProyectosElectiva1

--- jee-sample-step1/src/main/java/edu/uan/fis/jeesample/dao/impl/ProductDaoJdbc.java
+++ jee-sample-step1/src/main/java/edu/uan/fis/jeesample/dao/impl/ProductDaoJdbc.java
@@ -12,7 +12,7 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+//jhon alejandro leon sissa
 /**
  * Product DAO implementation using JDBC
  */
@@ -172,7 +172,7 @@ public class ProductDaoJdbc implements ProductDao {
         }
         return p;
     }
-    
+ 
     
     
     public List<Product> findAll() {
